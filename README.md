5677313144918e0dbe7604fa5254182d6a6ecc42
Drew Macrae <drewmacrae@gmail.com>
2026-08-15 08:25:21 -0400

    parameterize username and password

--- README.md
+++ README.md
@@ -3,9 +3,11 @@ Monitor for WIFI connections and start hotspot if not connected.
 # Installation
 Run `crontab -e` to add the line
 ```
-*/2 * * * * /usr/bin/python /home/pygu/headless/headless.py >> /home/pygu/headless/headless.log 2>&1
+*/2 * * * * /usr/bin/python /home/pygu/headless/headless.py $SSID $WPA_PASSWORD>> /home/pygu/headless/headless.log 2>&1
 ```
-to start monitoring and logging
+to start monitoring and logging.
+`$SSID` should specify the ID for the wifi hotspot, that will start if disconnected from WIFI.
+`$WPA_PASSWORD` should specify the password for the wifi hotspot
 
 # Updating wifi credentials
 After reconnecting to the hotspot presented by this raspberry pi, it can be assigned to join a new wifi network with
--- headless.py
+++ headless.py
@@ -1,3 +1,4 @@
+import argparse
 import sys
 import subprocess
 import time
@@ -5,7 +6,12 @@ import time
 SSID = "SIM7600NA"
 PASSWORD = "G9YDrfVdeavE"
 
-# for 2 minutes check if wifi is connected
+parser = argparse.ArgumentParser()
+parser.add_argument("ssid",help="SSID for wifi hotspot that will start if disconnected from WIFI",default=SSID)
+parser.add_argument("password",help="Passwrod for hotspot",default=PASSWORD)
+args = parser.parse_args()
+
+# for 1 minute check if wifi is connected
 for i in range(60):
     result = subprocess.check_output(["nmcli","-t"])
     for line in result.split(b"\n"):
@@ -18,6 +24,6 @@ for i in range(60):
     time.sleep(1)
 
 
-print("Starting a wifi hotspot! SSID",SSID+", password",PASSWORD)
+print("Starting a wifi hotspot! SSID",args.ssid+", password",args.password)
 # Start a wifi hotspot!
-subprocess.run(["sudo", "nmcli", "device", "wifi", "hotspot", "ssid", SSID, "password", PASSWORD, "ifname", "wlan0"])
+subprocess.run(["sudo", "nmcli", "device", "wifi", "hotspot", "ssid", args.ssid, "password", args.password, "ifname", "wlan0"])
